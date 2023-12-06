bcc20f54c9ee3e7c15cead2f4a14b81072ec39ae
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-06 19:40:04 +0000

    replace local pdf with pdfPath argument for crawler link up

--- scraper.js
+++ scraper.js
@@ -1,12 +1,9 @@
 const parseLocalPDF = require("./utils/scraper/processGeneralData")
 const insertSchoolData = require("./model/school_data")
 
-const scraper = async () => {
-    const pdfPath =
-      "/Users/eazzopardi/code/agency-scraper/sample report card (1).pdf"
-  
+const scraper = async (pdfPath) => {
     const pdfSchoolData = await parseLocalPDF(pdfPath)
     insertSchoolData(pdfSchoolData)
   }
   
-  scraper()
+scraper()
