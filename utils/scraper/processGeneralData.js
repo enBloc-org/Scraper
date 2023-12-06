fb96ef23c4d09542715f4ee68f38a7f7a02a4e32
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-06 15:09:08 +0000

    separate functions and convert all to arrow

--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -1,59 +1,9 @@
 const fs = require("fs")
 const pdf = require("pdf-parse")
 
-async function parseLocalPDF(pdfPath) {
-  const dataBuffer = fs.readFileSync(pdfPath)
-  // set page range as an option
-  const options = {
-    max: 1, // limit parsing to the first page only
-  }
-  const pdfdata = await pdf(dataBuffer, options)
+const mapVariablesToColumns = require('./mapVariablesToColumns')
 
-  const processedData = processGeneralData(pdfdata.text)
-  return processedData
-}
-const mapVariablesToColumns = {
-  State: "state",
-  District: "district",
-  Block: "block",
-  "Rural / Urban": "urban",
-  Cluster: "cluster",
-  Ward: "ward",
-  Mohalla: "mohalla",
-  Pincode: "pincode",
-  Panchayat: "panchayat",
-  City: "city",
-  Municipality: "municipality",
-  "School Category": "category",
-  "School Management": "management",
-  "Medium 1": "medium1",
-  "Medium 2": "medium2",
-  "Medium 3": "medium3",
-  "Medium 4": "medium4",
-  "Year of Establishment": "year_est",
-  "Is this a Shift School?": "shift",
-  "Anganwadi At Premises": "aganwadi",
-  "Year of Recognition-Pri.": "recog_pri",
-  "Building Status": "building_stat",
-  "Anganwadi Boys": "aganwadi_b",
-  "Year of Recognition-Upr.Pri.": "recog_upr_pri",
-  "Boundary Wall": "boundary",
-  "Anganwadi Girls": "aganwadi_g",
-  "Year of Recognition-Sec.": "recog_sec",
-  "No.of Building Blocks": "no_blocks",
-  "Anganwadi Worker": "aganwadi_worker",
-  "Year of Recognition-Higher Sec.": "recog_highsec",
-  "Pucca Building Blocks": "pucca_blocks",
-  "Residential School": "residential",
-  "Is Special School for CWSN?": "specialschool",
-  "Residential Type": "resid_type",
-  "Availability of Ramps": "ramps",
-  "Minority School": "minorityschool",
-  "Availability of Handrails": "handrails",
-  "Approachable By All Weather Road": "road",
-}
-
-function processGeneralData(pdfText) {
+const processGeneralData = (pdfText) => {
   const allValues = pdfText.split("\n")
   const variablesArr = [
     "State",
@@ -112,10 +62,6 @@ function processGeneralData(pdfText) {
       const value = allValues[i + 1]
       // Check that value exists in allValues but not in variablesArr. This is looking for a variable followed by another variable (and therefore an empty value)
       if (!variablesArr.some(variable => value.includes(variable))) {
-        // Log variable : value pair
-        // console.log(splitWords, " : ", value)
-
-        // prepare the data for schoolDataArr
         const columns = mapVariablesToColumns[splitWords]
         const dataObject = {}
         dataObject[columns] = value
@@ -124,6 +70,18 @@ function processGeneralData(pdfText) {
     }
   })
   return schoolDataArr
+}
+
+const parseLocalPDF = async (pdfPath) => {
+  const dataBuffer = fs.readFileSync(pdfPath)
+
+  // temporarily limit parsing to the first page only
+  const options = {
+    max: 1, 
+  }
+  const pdfdata = await pdf(dataBuffer, options)
+  const processedData = processGeneralData(pdfdata.text)
+  return processedData
 }
 
 module.exports = parseLocalPDF
