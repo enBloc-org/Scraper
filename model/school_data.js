f6fd7830a974de5106639844fd187073c96fc111
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-07 11:27:03 +0000

    restructure Object.entries to avoid unused vars

--- model/school_data.js
+++ model/school_data.js
@@ -1,21 +1,19 @@
-/* eslint-disable no-unused-vars */
-
 const db = require("../database/school_data_db")
 
 const insertSchoolData = async schoolData => {
   const columns = schoolData
     .map(obj =>
       Object.entries(obj)
-        .filter(([key, value]) => key !== "undefined")
-        .map(([key, value]) => key),
+        .filter(([key]) => key !== "undefined")
+        .map(([key]) => key),
     )
     .flat()
 
   const data = schoolData
     .map(obj =>
       Object.entries(obj)
-        .filter(([key, value]) => key !== "undefined")
-        .map(([key, value]) => value),
+        .filter(([key]) => key !== "undefined")
+        .map(([, value]) => value),
     )
     .flat()
 
