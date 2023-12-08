67ebc3ba1b09e97f27bdb05a66c8d02c3a8a2e11
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-08 11:38:58 +0000

    add string to variablesArr to avoid it being processed as value

--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -3,31 +3,36 @@ const pdf = require("pdf-parse")
 
 const mapVariablesToColumns = require("./mapVariablesToColumns")
 const variablesArr = Object.keys(mapVariablesToColumns)
+variablesArr.push("Visit of school for / by") 
 
 const processGeneralData = async pdfText => {
-  
   const allValues = pdfText.split("\n")
+  // udise_code and schoolname are not split by \n so need an alternative process
   const schoolDataArr = []
   
   allValues.forEach((word, i) => {
-
     const splitPoint = word.search(/[a-z][A-Z]/)
+
     let splitWord = word
-    
+
     if (splitPoint !== -1) {
       splitWord = word.substring(splitPoint + 1)
+
     }
-    
-    if (variablesArr.some(variable => splitWord.includes(variable))) {
+
+    if (variablesArr.some(variable => variable.includes(splitWord))) {
+
       const value = allValues[i + 1]
+
+      
       if (!variablesArr.includes(value)) {
         const columns = mapVariablesToColumns[splitWord]
-        const dataObject = { [columns]: value };
+        const dataObject = { [columns]: value }
         schoolDataArr.push(dataObject)
       }
     }
   })
-  
+
   return schoolDataArr
 }
 
@@ -41,7 +46,5 @@ const parseLocalPDF = async pdfPath => {
   const processedData = processGeneralData(pdfdata.text)
   return processedData
 }
-
-
 
 module.exports = parseLocalPDF
