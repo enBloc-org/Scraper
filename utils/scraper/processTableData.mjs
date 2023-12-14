c3d107b7be6b62e48c1cb517add167cbccf5810c
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-14 17:51:49 +0000

    refactor into smaller functions to avoid nesting

--- utils/scraper/processTableData.mjs
+++ utils/scraper/processTableData.mjs
@@ -1,50 +1,65 @@
 import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs"
 import enrolment_and_minority from "./coordinates.mjs"
 
-function extractText(pdfUrl) {
-  const loadingTask = getDocument(pdfUrl)
-  loadingTask.promise
-    .then(async pdf => {
-      const countPromises = []
-      countPromises.push(
-        pdf.getPage(2).then(async page => {
-          const textContent = await page.getTextContent()
-          return textContent.items.map(item => ({
-            text: item.str,
-            x: item.transform[4], // x-coord
-            y: item.transform[5], // y-coord
-          }))
-        }),
-      )
-
-      const texts = await Promise.all(countPromises)
-      return texts.flat()
-    })
-    .then(items => {
-      for (const row in enrolment_and_minority) {
-        if (row !== 'grade' && enrolment_and_minority.hasOwnProperty(row)) {
-          const y = enrolment_and_minority[row];
-          for (const grade in enrolment_and_minority.grade) {
-            if (enrolment_and_minority.grade.hasOwnProperty(grade)) {
-              const gradeData = enrolment_and_minority.grade[grade];
-              for (const col in gradeData) {
-                if (gradeData.hasOwnProperty(col)) {
-                  const { xmin, xmax } = gradeData[col];
-                  items.forEach(item => {
-                    if (item.x >= xmin && item.x <= xmax && Math.abs(item.y - y) <= 1) {
-                      console.log(`${row}_${grade}_${col.toLowerCase()}: ${item.text}`);
-                    }
-                  });
-                }
-              }
-            }
-          }
-        }
-	}
-})
-    .catch(err => {
-      console.error(`Error: ${err}`)
-    })
+const processDocument = (pdf) => {
+  return pdf.getPage(2).then(page => page.getTextContent());
 }
+
+const processColumn = (item, gradeData, grade, y) => {
+  for (const col in gradeData) {
+    if (gradeData.hasOwnProperty(col)) {
+      const { xmin, xmax } = gradeData[col];
+      if (item.x >= xmin && item.x <= xmax && Math.abs(item.y - y) <= 1) {
+        console.log(`${grade}_${col}: ${item.text}`);
+      }
+    }
+  }
+}
+
+
+const processGrade = (item, grades, y) => {
+  for (const grade in grades) {
+    if (grades.hasOwnProperty(grade)) {
+      const gradeData = grades[grade];
+      processColumn(item, gradeData, grade, y);
+    }
+  }
+}
+
+const processItem = (item, obj) => {
+  for (const row in obj) {
+    if (row !== 'grade' && obj.hasOwnProperty(row)) {
+      const y = obj[row];
+      processGrade(item, obj.grade, y);
+    }
+  }
+}
+
+const filterItems = (textContent) => {
+  const items = textContent.items.map(item => ({
+    text: item.str,
+    x: item.transform[4], // x-coordinate
+    y: item.transform[5]  // y-coordinate
+  }));
+
+
+
+  items.forEach(item => {
+    processItem(item, enrolment_and_minority);
+  });
+}
+
+
+
+const extractText = (url) => {
+  const loadingTask = getDocument(url);
+  loadingTask.promise
+    .then(processDocument)
+    .then(filterItems)
+    .catch(err => {
+      console.error(`Error: ${err}`);
+    });
+}
+
 
 extractText("/Users/eazzopardi/code/agency-scraper/sample report card (1).pdf")
