e37eb3f64e106548f9e9010adcf9ad37ef75f930
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-18 12:40:33 +0000

    update scraper to es6

--- scraper.js
+++ scraper.js
@@ -1,9 +1,16 @@
-const parseLocalPDF = require("./utils/scraper/processGeneralData")
-const insertSchoolData = require("./model/school_data")
+import parseLocalPDF from "./utils/scraper/processGeneralData.js"
+import processTableData from "./utils/scraper/processTableData.js"
+import insertSchoolData from "./model/school_data.js"
+import insertTableData from "./model/table_data.js"
 
 const scraper = async pdfPath => {
   const pdfSchoolData = await parseLocalPDF(pdfPath)
   insertSchoolData(pdfSchoolData)
 }
 
-scraper()
+// const tableData = processTableData(
+//   "/Users/eazzopardi/code/agency-scraper/sample report card (1).pdf",
+// )
+// insertTableData(tableData)
+
+
