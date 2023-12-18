7abe8dd6e4e9886c73990db2e2714cb98aa25b1f
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-18 12:32:35 +0000

    update school_data_db.js to es6

--- database/school_data_db.js
+++ database/school_data_db.js
@@ -1,10 +1,10 @@
-const { readFileSync } = require("node:fs")
-const { join } = require("node:path")
-const Database = require("better-sqlite3")
+import { readFileSync } from "node:fs"
+import { join } from "node:path"
+import Database from "better-sqlite3"
 
 const db = new Database("school_data.sql")
 const schemaPath = join("database", "school_data.sql")
 const schema = readFileSync(schemaPath, "utf-8")
 db.exec(schema)
 
-module.exports = db
+export default db
