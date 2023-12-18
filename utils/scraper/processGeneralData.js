c959e4b41c72108780b1df216331556e2213f6be
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-18 15:29:07 +0000

    refactor processGeneralData and use regex for udise code

--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -6,10 +6,45 @@ import variables from "./variables.js"
 const variablesArr = Object.keys(variables)
 variablesArr.push("Visit of school for / by")
 
-const processGeneralData = async pdfText => {
-  const allValues = pdfText.split("\n")
-  // udise_code and schoolname are not split by \n so need an alternative process
+const parseDocument = async pdfPath => {
+  const dataBuffer = readFileSync(pdfPath);
+  const options = {
+    max: 1,
+  };
+
+  try {
+    const pdfdata = await pdf(dataBuffer, options)
+    return pdfdata
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+};
+
+const processUdiseCode = (data) => {
+  const regex = /UDISE CODE(.*?)School Name/g;
+  const match = regex.exec(data[3]);
+  if (match && match[1]) {
+    const udise_code = { udise_code : match[1].trim() } // Trim any leading/trailing spaces
+    return udise_code;
+  }
+  else return null
+}
+
+
+const processGeneralData = async pdfPath => {
   const schoolDataArr = []
+
+  const parsedpdf = await parseDocument(pdfPath)
+  const pdftext = parsedpdf.text
+
+  const allValues = pdftext.split("\n")
+  
+  const udise_code = processUdiseCode(allValues)
+  schoolDataArr.push(udise_code)
+
+  // udise_code and schoolname are not split by \n so need an alternative process
+  
   allValues.forEach((word, i) => {
     const splitPoint = word.search(/[a-z][A-Z]/)
 
@@ -29,19 +64,8 @@ const processGeneralData = async pdfText => {
       }
     }
   })
-
   return schoolDataArr
 }
 
-const parseLocalPDF = async pdfPath => {
-  const dataBuffer = readFileSync(pdfPath)
-  const options = {
-    max: 1,
-  }
-  const pdfdata = await pdf(dataBuffer, options)
-  const processedData = processGeneralData(pdfdata.text)
-  return processedData
-  
-}
 
-export default parseLocalPDF
+export default processGeneralData
