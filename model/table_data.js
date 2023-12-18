2c3228514e94203045ebbb9056b3557b4dfe9e5c
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-18 12:56:18 +0000

    import processedTableData for db insert

--- model/table_data.js
+++ model/table_data.js
@@ -1,7 +1,13 @@
-import db from "../database/school_data_db.js"
+// import db from "../database/school_data_db.js"
+import processTableData from "../utils/scraper/processTableData.js"
+
+const processedTableData = await processTableData("/Users/eazzopardi/code/agency-scraper/school report card 2.pdf")
 
 const insertTableData = async tableData => {
-  const columns = tableData.map(obj => console.log(Object.entries(obj)))
+  console.log(tableData)
+  // const columns = tableData.map(obj => console.log(Object.entries(obj)))
 }
+
+insertTableData(processedTableData )
 
 export default insertTableData
