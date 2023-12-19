fb567c5777ddf69e100dd6159053e893e7560ae1
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-19 16:58:33 +0000

    add functions for schoolname and udise_code

--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -7,11 +7,10 @@ const variablesArr = Object.keys(variables)
 variablesArr.push("Visit of school for / by")
 
 const parseDocument = async pdfPath => {
-  const dataBuffer = readFileSync(pdfPath);
+  const dataBuffer = readFileSync(pdfPath)
   const options = {
     max: 1,
-  };
-
+  }
   try {
     const pdfdata = await pdf(dataBuffer, options)
     return pdfdata
@@ -19,18 +18,25 @@ const parseDocument = async pdfPath => {
     console.error(error)
     throw error
   }
-};
-
-const processUdiseCode = (data) => {
-  const regex = /UDISE CODE(.*?)School Name/g;
-  const match = regex.exec(data[3]);
-  if (match && match[1]) {
-    const udise_code = { udise_code : match[1].trim() } // Trim any leading/trailing spaces
-    return udise_code;
-  }
-  else return null
 }
 
+const udiseCode = data => {
+  const regex = /UDISE CODE(.*?)School Name/g
+  const match = regex.exec(data[3])
+  if (match && match[1]) {
+    const udise_code = { udise_code: match[1].trim() } // Trim any leading/trailing spaces
+    return udise_code
+  } else return null
+}
+
+const schoolName = data => {
+  const regex = /School Name(.*?)$/
+  const match = regex.exec(data[3])
+  if (match && match[1]) {
+    const schoolname = { schoolname: match[1].trim() } // Trim any leading/trailing spaces
+    return schoolname
+  } else return null
+}
 
 const processGeneralData = async pdfPath => {
   const schoolDataArr = []
@@ -39,12 +45,13 @@ const processGeneralData = async pdfPath => {
   const pdftext = parsedpdf.text
 
   const allValues = pdftext.split("\n")
-  
-  const udise_code = processUdiseCode(allValues)
+
+  const udise_code = udiseCode(allValues)
   schoolDataArr.push(udise_code)
 
-  // udise_code and schoolname are not split by \n so need an alternative process
-  
+  const schoolname = schoolName(allValues)
+  schoolDataArr.push(schoolname)
+
   allValues.forEach((word, i) => {
     const splitPoint = word.search(/[a-z][A-Z]/)
 
@@ -66,6 +73,5 @@ const processGeneralData = async pdfPath => {
   })
   return schoolDataArr
 }
-
 
 export default processGeneralData
