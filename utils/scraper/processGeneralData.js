293d098916aa2b953b529e077423d44649a8d63e
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-22 14:51:50 +0000

    work on typos

--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -1,74 +1,108 @@
-import { readFileSync } from "fs";
-import pdf from "pdf-parse";
-import variables from "./variables.js";
+import { readFileSync } from "fs"
+import pdf from "pdf-parse"
+import variables from "./variables.js"
 
-const variablesArr = Object.keys(variables);
-variablesArr.push("Visit of school for / by");
+const variablesArr = Object.keys(variables)
+variablesArr.push("Visit of school for / by")
 
-const parseDocument = async (pdfPath) => {
-  const dataBuffer = readFileSync(pdfPath);
+const fuzzyMatch = (input, array) => {
+  const regexString = input
+    .split('')
+    .map(char => `[${char}]`)
+    .join('.*?');
+
+  const regex = new RegExp(regexString, 'i'); // 'i' flag for case-insensitive match
+
+  return array.filter(item => regex.test(item));
+};
+
+const parseDocument = async pdfPath => {
+  const dataBuffer = readFileSync(pdfPath)
   const options = {
     max: 1,
-  };
-  try {
-    const pdfdata = await pdf(dataBuffer, options);
-    return pdfdata;
-  } catch (error) {
-    console.error(error);
-    throw error;
   }
-};
+  try {
+    const pdfdata = await pdf(dataBuffer, options)
+    return pdfdata
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
 const extractValue = (data, regexPattern) => {
-  const regex = new RegExp(regexPattern);
-  const match = regex.exec(data[3]);
+  const regex = new RegExp(regexPattern)
+  const match = regex.exec(data[3])
   if (match && match[1]) {
-    return match[1].trim(); // Trim any leading/trailing spaces
+    return match[1].trim() // Trim any leading/trailing spaces
   } else {
-    return "*";
+    return "*"
   }
-};
+}
 
-const processGeneralData = async (pdfPath) => {
-  const schoolDataArr = [];
-  const parsedpdf = await parseDocument(pdfPath);
-  const pdftext = parsedpdf.text;
+const processGeneralData = async pdfPath => {
+  const schoolDataArr = []
+  const parsedpdf = await parseDocument(pdfPath)
+  const pdftext = parsedpdf.text
 
   const all = pdftext.split("\n")
 
-  const udiseRegex = /UDISE CODE(.*?)School Name/g;
-  const schoolRegex = /School Name(.*?)$/;
+  const udiseRegex = /UDISE CODE(.*?)School Name/g
+  const schoolRegex = /School Name(.*?)$/
 
-  const udise_code = { udise_code: extractValue(all, udiseRegex) };
-  const schoolname = { schoolname: extractValue(all, schoolRegex) };
+  const udise_code = { udise_code: extractValue(all, udiseRegex) }
+  const schoolname = { schoolname: extractValue(all, schoolRegex) }
 
-  schoolDataArr.push(udise_code, schoolname);
+  schoolDataArr.push(udise_code, schoolname)
 
   for (let i = 0; i < all.length; i++) {
-    const word = all[i];
-    const value = all[i + 1];
-    
-    if (word === 'DigiBoard' && variablesArr.some(variable => variable.includes(word))) {
-      const columns = variables[word];
-      const dataObject = { [columns]: value };
-      schoolDataArr.push(dataObject);
-    } else {
-      const splitPoint = word.search(/[a-z][A-Z]/);
-      let splitWord = word;
+    const word = all[i]
+    const value = all[i + 1]
+
+    // DIGIBOARD 
+    if ( word === "DigiBoard" && variablesArr.some(variable => variable.includes(word))) {
+      const columns = variables[word]
+      const dataObject = { [columns]: value }
+      schoolDataArr.push(dataObject)
+      
+    } 
+    else {
+      // ALL OTHER VALUES 
+      const splitPoint = word.search(/[a-z][A-Z]/)
+      // console.log("word: ", word)
+      let splitWord = word
+      const matchingVariables = fuzzyMatch(splitWord, variablesArr);
 
       if (splitPoint !== -1) {
-        splitWord = word.substring(splitPoint + 1);
+        splitWord = word.substring(splitPoint + 1)
+        // console.log("splitWord: ", splitWord)
       }
+      
+      if (
+        variablesArr.some(variable => variable.includes(splitWord)) &&
+        !variablesArr.includes(value) 
+      ) {
+        
+        const columns = variables[splitWord]
+        const dataObject = { [columns]: value }
+        schoolDataArr.push(dataObject)
 
-      if (variablesArr.some(variable => variable.includes(splitWord)) && !variablesArr.includes(value)) {
-        const columns = variables[splitWord];
-        const dataObject = { [columns]: value };
-        schoolDataArr.push(dataObject);
       }
+      // TYPOS
+      
+      else if (matchingVariables.length > 0 && !variablesArr.includes(value)) {
+        const columns = variables[matchingVariables];
+        if (!schoolDataArr.some(data => data.hasOwnProperty(columns))) {
+          console.log(columns);
+          const dataObject = { [columns]: value };
+          console.log(dataObject)
+          //   schoolDataArr.push(dataObject);
+        }
+      } 
     }
   }
 
-  return schoolDataArr;
-};
+  return schoolDataArr
+}
 
-export default processGeneralData;
+export default processGeneralData
