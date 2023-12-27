c33f9ca7226768a0b36dfef97ff58e9716b86b03
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2023-12-27 11:43:40 +0000

    Revert "gathering year information from pdf title to add to scraped data"
    This reverts commit 2426c8fa536f8aad8d11480016fc1af9a091e4f6.

--- scraper.js
+++ scraper.js
@@ -1,8 +1,7 @@
-import path from "path"
-
 import processGeneralData from "./utils/scraper/processGeneralData.js"
 import processTableData from "./utils/scraper/processTableData.js"
 import insertSchoolData from "./model/school_data.js"
+import path from "path"
 
 const scraper = async pdfPath => {
   const pdfSchoolData = await processGeneralData(pdfPath)
