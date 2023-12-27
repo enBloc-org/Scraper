7b5243708a86a2c3fbc0d2a773e1fbc195785006
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2023-12-27 16:18:29 +0000

    moved logic to getYearValue withing processGeneralData for separation of concerns

--- scraper.js
+++ scraper.js
@@ -1,19 +1,11 @@
-import path from "path"
-
 import processGeneralData from "./utils/scraper/processGeneralData.js"
 import processTableData from "./utils/scraper/processTableData.js"
 import insertSchoolData from "./model/school_data.js"
-
-const yearRegex = /(2018-19)|(2019-20)|(2020-21)|(2021-22)|(2022-23)/
 
 const scraper = async pdfPath => {
   const pdfSchoolData = await processGeneralData(pdfPath)
   const tableData = await processTableData(pdfPath)
   pdfSchoolData.push(tableData)
-
-  const fileBaseTitle = path.basename(pdfPath)
-  const year = fileBaseTitle.match(yearRegex)[0]
-  pdfSchoolData.push({ year })
 
   insertSchoolData(pdfSchoolData)
 
--- utils/scraper/processGeneralData.js
+++ utils/scraper/processGeneralData.js
@@ -1,74 +1,89 @@
-import { readFileSync } from "fs";
-import pdf from "pdf-parse";
-import variables from "./variables.js";
+import { readFileSync } from "fs"
+import path from "path"
+import pdf from "pdf-parse"
+import variables from "./variables.js"
 
-const variablesArr = Object.keys(variables);
-variablesArr.push("Visit of school for / by");
+const variablesArr = Object.keys(variables)
+variablesArr.push("Visit of school for / by")
 
-const parseDocument = async (pdfPath) => {
-  const dataBuffer = readFileSync(pdfPath);
+const parseDocument = async pdfPath => {
+  const dataBuffer = readFileSync(pdfPath)
   const options = {
     max: 1,
-  };
-  try {
-    const pdfdata = await pdf(dataBuffer, options);
-    return pdfdata;
-  } catch (error) {
-    console.error(error);
-    throw error;
   }
-};
+  try {
+    const pdfdata = await pdf(dataBuffer, options)
+    return pdfdata
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
 const extractValue = (data, regexPattern) => {
-  const regex = new RegExp(regexPattern);
-  const match = regex.exec(data[3]);
+  const regex = new RegExp(regexPattern)
+  const match = regex.exec(data[3])
   if (match && match[1]) {
-    return match[1].trim(); // Trim any leading/trailing spaces
+    return match[1].trim() // Trim any leading/trailing spaces
   } else {
-    return "*";
+    return "*"
   }
-};
+}
 
-const processGeneralData = async (pdfPath) => {
-  const schoolDataArr = [];
-  const parsedpdf = await parseDocument(pdfPath);
-  const pdftext = parsedpdf.text;
+const getYearValue = filePath => {
+  const yearRegex = /(2018-19)|(2019-20)|(2020-21)|(2021-22)|(2022-23)/
+  const fileBaseTitle = path.basename(filePath)
+  
+  return fileBaseTitle.match(yearRegex)[0]
+}
+
+const processGeneralData = async pdfPath => {
+  const schoolDataArr = []
+  const parsedpdf = await parseDocument(pdfPath)
+  const pdftext = parsedpdf.text
 
   const all = pdftext.split("\n")
 
-  const udiseRegex = /UDISE CODE(.*?)School Name/g;
-  const schoolRegex = /School Name(.*?)$/;
+  const udiseRegex = /UDISE CODE(.*?)School Name/g
+  const schoolRegex = /School Name(.*?)$/
 
-  const udise_code = { udise_code: extractValue(all, udiseRegex) };
-  const schoolname = { schoolname: extractValue(all, schoolRegex) };
+  const udise_code = { udise_code: extractValue(all, udiseRegex) }
+  const schoolname = { schoolname: extractValue(all, schoolRegex) }
+  const year = { year: getYearValue(pdfPath) }
 
-  schoolDataArr.push(udise_code, schoolname);
+  schoolDataArr.push(udise_code, schoolname, year)
 
   for (let i = 0; i < all.length; i++) {
-    const word = all[i];
-    const value = all[i + 1];
-    
-    if (word === 'DigiBoard' && variablesArr.some(variable => variable.includes(word))) {
-      const columns = variables[word];
-      const dataObject = { [columns]: value };
-      schoolDataArr.push(dataObject);
+    const word = all[i]
+    const value = all[i + 1]
+
+    if (
+      word === "DigiBoard" &&
+      variablesArr.some(variable => variable.includes(word))
+    ) {
+      const columns = variables[word]
+      const dataObject = { [columns]: value }
+      schoolDataArr.push(dataObject)
     } else {
-      const splitPoint = word.search(/[a-z][A-Z]/);
-      let splitWord = word;
+      const splitPoint = word.search(/[a-z][A-Z]/)
+      let splitWord = word
 
       if (splitPoint !== -1) {
-        splitWord = word.substring(splitPoint + 1);
+        splitWord = word.substring(splitPoint + 1)
       }
 
-      if (variablesArr.some(variable => variable.includes(splitWord)) && !variablesArr.includes(value)) {
-        const columns = variables[splitWord];
-        const dataObject = { [columns]: value };
-        schoolDataArr.push(dataObject);
+      if (
+        variablesArr.some(variable => variable.includes(splitWord)) &&
+        !variablesArr.includes(value)
+      ) {
+        const columns = variables[splitWord]
+        const dataObject = { [columns]: value }
+        schoolDataArr.push(dataObject)
       }
     }
   }
 
-  return schoolDataArr;
-};
+  return schoolDataArr
+}
 
-export default processGeneralData;
+export default processGeneralData
