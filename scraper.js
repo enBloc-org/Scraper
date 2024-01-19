fa4313273588dc1ad857162d05343b953330b653
eliazzo <114364165+eliazzo@users.noreply.github.com>
2024-01-19 10:52:11 +0000

    rewrite comments for clarity

--- scraper.js
+++ scraper.js
@@ -1,9 +1,13 @@
-import { processGeneralData, getNameValue, getYearValue } from "./utils/scraper/processGeneralData.js"
+import {
+  processGeneralData,
+  getNameValue,
+  getYearValue,
+} from "./utils/scraper/processGeneralData.js"
 import processTableData from "./utils/scraper/processTableData.js"
 import insertSchoolData from "./model/school_data.js"
 
 const scraper = async pdfPath => {
-  // CORE VALUES
+  // SCHOOL NAME AND YEAR VALUES
   const schoolname = { schoolname: getNameValue(pdfPath) }
   const year = { year: getYearValue(pdfPath) }
 
