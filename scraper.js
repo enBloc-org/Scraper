2bd3edbde3f031975c2c1d851c606061784be38b
eliazzo <114364165+eliazzo@users.noreply.github.com>
2024-01-19 10:51:32 +0000

    update scraper function with new udise changes

--- scraper.js
+++ scraper.js
@@ -1,11 +1,19 @@
-import processGeneralData from "./utils/scraper/processGeneralData.js"
+import { processGeneralData, getNameValue, getYearValue } from "./utils/scraper/processGeneralData.js"
 import processTableData from "./utils/scraper/processTableData.js"
 import insertSchoolData from "./model/school_data.js"
 
 const scraper = async pdfPath => {
+  // CORE VALUES
+  const schoolname = { schoolname: getNameValue(pdfPath) }
+  const year = { year: getYearValue(pdfPath) }
+
+  // GENERAL VALUES
   const pdfSchoolData = await processGeneralData(pdfPath)
+
+  // UDISE AND TABLE VALUES
   const tableData = await processTableData(pdfPath)
-  pdfSchoolData.push(tableData)
+
+  pdfSchoolData.push(schoolname, year, tableData)
 
   insertSchoolData(pdfSchoolData)
 
